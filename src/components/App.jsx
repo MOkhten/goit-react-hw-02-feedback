25dbe7e76f2f6cf8642a15c1ab52de1c1f338b35
MOkhten <m.okhten@gmail.com>
2022-12-27 22:39:51 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+
+export class App  extends Component {
+    state = {
+        good: 0,
+        neutral: 0,
+        bad: 0
+  };
+  
+  leaveFeedback = key => {
+    this.setState(prevState => ({
+      [key]: prevState[key] + 1,
+    }));
+  };
+    render() {
+        const { good, neutral, bad } = this.state;
+        const totalFeedbacks = this.state.good + this.state.neutral + this.state.bad;
+        return (
+            <div>
+                <div>
+                <h1>Please leave a feedback</h1>
+              <button options={['good', 'neutral', 'bad']} onLeaveFeedback={this.leaveFeedback}></button>
+                </div>
+                <div>
+                    <h2>Statistics</h2>
+               good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.state.totalFeedback}
+              
+                </div>
+            </div >
+        )
+    }
+}
